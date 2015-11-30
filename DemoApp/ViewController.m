e8240796a262769198dd8304d5b41a740f0f2533
Kazuya Hirobe <k.hirobe@gmail.com>
2015-11-30 15:01:55 +0900

    Add DemoApp files.

---
+++ DemoApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RPLView
+//
+//  Created by Hirobe Kazuya on 11/30/15.
+//  Copyright © 2015 Bunguu inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ RPLView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RPLView
+//
+//  Created by Hirobe Kazuya on 11/30/15.
+//  Copyright © 2015 Bunguu inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
